427334219fb2ac075e18290396bdda7a144f50ba
zhuojunxu <xuzhuojun1128@126.com>
2023-05-22 11:53:50 -0400

    Fixed the issue for working eraser with Color, next step maybe either add Rainbow Mode, or enhance the mouse functionality too.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,8 @@
 const grids = document.querySelector('.grids');
 let dimension = document.getElementById("intro");
 let submission = document.getElementById("confirm");
-//let eraser = document.getElementById("eraser");
-//let colorMode = document.getElementById("colorMode");
+let eraser = document.getElementById("eraser");
+let colorMode = document.getElementById("colorMode");
 let clear = document.getElementById("clear");
 
 let colorButtons = document.getElementsByClassName('modes');
@@ -23,6 +23,7 @@ function getDimension() {
     return document.getElementById("dim").innerHTML = dimension.value + '*' + dimension.value;
 }
 
+colorMode.addEventListener("click", colorGrid);
 // Code reserve to get either the color or eraser.
 /*function getColorState() {
     if (colorMode) {
@@ -47,7 +48,6 @@ function drawGrid(grids, dimension3) {
             square.classList.add('gridColumn');
             square.setAttribute("id", "square");
             row.appendChild(square);
-            active = true;
             grids.addEventListener('mouseover', getColor);
         }
     }
@@ -72,18 +72,25 @@ clear.addEventListener("click", (clear) => clearGrid());
 //}*/
 
 
-function eraseGrid() {
-    grids.addEventListener('mouseover', getWhiteColor);
-}
 
 function colorGrid() {
+    eraserStatus = false;
     grids.addEventListener('mouseover', getColor);
+    grids.removeEventListener('mouseover', getWhiteColor);
+}
+
+function eraseGrid() {
+    eraserStatus = true;
+    grids.addEventListener('mouseover', getWhiteColor);
+    grids.removeEventListener('mouseover', getColor);
 }
 
 
 function getColor(event) {
+    console.log("Trying");
     let square = event.target.style;
-    const color = document.getElementById("color-picker").value;
+    let color = document.getElementById("color-picker").value;
+    console.log(color);
     return square.backgroundColor = color;
 }
 
@@ -108,3 +115,4 @@ function resizeGrid(grids2, dimension3) {
     drawGrid(grids3, dimension3);
 }
 
+eraser.addEventListener("click", eraseGrid);
